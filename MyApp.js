8b4e73eafec596915f19babf423d1c35d6ea536f
hzy82218052 <82218052@qq.com>
2018-08-02 18:21:27 +0800

    第二次提交 commit

--- MyApp.js
+++ MyApp.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Platform,
     StyleSheet,
@@ -17,29 +17,97 @@ import Item from './components/Item';
 import movies from './movies';
 
 const styles = StyleSheet.create({
-    row:{
+    row: {
         // flexDirection:'row',
         // justifyContent:'space-between',
-        paddingHorizontal:15,
+        paddingHorizontal: 15,
     }
 })
+//https://api.douban.com/v2/movie/in_theaters
+
+const api = 'https://api.douban.com/v2/movie/in_theaters';
 
 export default class MyApp extends Component<Props> {
+    state = {
+        movies: movies.subjects,
+        refreshing: false,
+    }
+
+    refreshing = false;
+    start = 0;
+    count = 12;
+    fetchData = (start = 0, count = 12) => {
+        if (this.refreshing) {
+            return
+        }
+        this.setState({
+            refreshing: true,
+        });
+        this.refreshing = true;
+        //1边上的点
+        return fetch(`${api}?start=${start}&count=${count}`)
+            .then((response) => response.text())
+            .then((responseText) => {
+                console.log("fetch:" + responseText);
+                const json = JSON.parse(responseText);
+                return json;
+            })
+            .then((responseJson) => {
+                this.setState({
+                    refreshing: false,
+                });
+                this.refreshing = false;
+                return responseJson;
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    };
+
+    freshData = async () => {
+        // this.fetchData().then(json=>{
+        //      this.setState({
+        //          movies: responseJson.subjects,
+        //      })
+        //  });
+        const json = await this.fetchData();
+        this.setState({
+            movies: json.subjects,
+        });
+    }
+    fetchMore = async () => {
+        const json = await this.fetchData(this.start,this.count);
+        this.start +=this.count-1;
+        this.setState({
+            movies: this.state.movies.concat(json.subjects),
+        });
+    }
+
+    componentDidMount() {
+        // this.fetchData();
+    };
+
+
     render() {
+        const {movies, refreshing} = this.state;
         return (
-            <View >
+            <View>
                 <FlatList
                     numColumns={3}
                     columnWrapperStyle={styles.row}
                     keyExtractor={item => item.id}
-                    data={movies.subjects}
-                    renderItem={({item}) =>
-                        <Item
+                    data={movies}
+                    onRefresh={this.freshData}
+                    onEndReached={this.fetchMore}
+                    onEndReachedThreshold={0}
+                    refreshing={refreshing}
+                    renderItem={({item}) =>{
+                        return (<Item
                             title={item.title}
                             image={item.images.medium}
                             stars={item.rating.stars}
                             average={item.rating.average}
-                        />
+                        />)}
                     }
                 />
             </View>
